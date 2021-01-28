7bb4674eac6667e63c0f6e7ae7027328f3926338
okan08 <okan08@users.noreply.github.com>
2021-01-29 07:51:01 +0900

    アソシエーション設定

--- app/models/snowresort.rb
+++ app/models/snowresort.rb
@@ -1,7 +1,12 @@
 class Snowresort < ApplicationRecord
   belongs_to :user
-  #has_many :comments
+  has_many :comments
   has_one_attached :image
 
-  
+  with_options presence: true do
+    validates :resort_name
+    validates :introduction
+    validates :image
+    validates :details
+  end
 end
