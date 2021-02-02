4467c5e5670fda9a350d44d7ad859b8dcb2ff2fd
okan08 <okan08@users.noreply.github.com>
2021-02-02 23:31:26 +0900

    ネストの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "snowresorts#index"
-  resources :snowresorts
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :snowresorts do
+    resources :comments
+  end
 end
